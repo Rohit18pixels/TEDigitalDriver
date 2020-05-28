d35e0b046d352549e8296b043614e361cf78d5d7
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-05-28 12:37:40 +0530

    Livingtheapp

--- app/src/main/java/com/livingtheapp/user/auth/AuthMainActivity.java
+++ app/src/main/java/com/livingtheapp/user/auth/AuthMainActivity.java
@@ -3,6 +3,8 @@ package com.livingtheapp.user.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.livingtheapp.user.R;
 
@@ -11,6 +13,8 @@ public class AuthMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_auth_main);
 
