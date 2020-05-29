774af6737fc3bf6e17a87df4e81129cb8ae91c84
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-05-29 11:50:03 +0530

    Livingtheapp

--- app/src/main/java/com/livingtheapp/user/auth/RegistrationActivity.java
+++ app/src/main/java/com/livingtheapp/user/auth/RegistrationActivity.java
@@ -2,9 +2,20 @@ package com.livingtheapp.user.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.livingtheapp.user.R;
+import com.livingtheapp.user.utils.AppUrl;
+
+import org.json.JSONObject;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -13,5 +24,28 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_registration);
+
+        getExecuteMethods();
+    }
+
+    void getExecuteMethods()
+    {
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, AppUrl.getCountriesDataService,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(RegistrationActivity.this);
+        request.setRetryPolicy(new DefaultRetryPolicy(20*2000, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(request);
     }
 }
---
+++ app/src/main/java/com/livingtheapp/user/utils/AppUrl.java
@@ -0,0 +1,7 @@
+package com.livingtheapp.user.utils;
+
+public class AppUrl {
+
+    public static String BASE_URL= "http://living.indo3dworld.com/api/";
+    public static String getCountriesDataService= BASE_URL+"getCountriesDataService";
+}
