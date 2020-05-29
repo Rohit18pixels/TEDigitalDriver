18b8e1087d2bc7863984c6efc00ef0e625ba8a14
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-05-29 12:04:22 +0530

    Livingtheapp

---
+++ app/src/main/java/com/livingtheapp/user/auth/ModalCountries.java
@@ -0,0 +1,59 @@
+package com.livingtheapp.user.auth;
+
+public class ModalCountries {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryStatus() {
+        return countryStatus;
+    }
+
+    public void setCountryStatus(String countryStatus) {
+        this.countryStatus = countryStatus;
+    }
+
+    public String getCountryFlag() {
+        return countryFlag;
+    }
+
+    public void setCountryFlag(String countryFlag) {
+        this.countryFlag = countryFlag;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCallingCode() {
+        return callingCode;
+    }
+
+    public void setCallingCode(String callingCode) {
+        this.callingCode = callingCode;
+    }
+
+    public String id ;
+    public String countryName ;
+    public String countryStatus ;
+    public String countryFlag ;
+    public String countryCode ;
+    public String callingCode ;
+}
--- app/src/main/java/com/livingtheapp/user/auth/RegistrationActivity.java
+++ app/src/main/java/com/livingtheapp/user/auth/RegistrationActivity.java
@@ -1,9 +1,13 @@
 package com.livingtheapp.user.auth;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -15,37 +19,75 @@ import com.android.volley.toolbox.Volley;
 import com.livingtheapp.user.R;
 import com.livingtheapp.user.utils.AppUrl;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    ArrayList<ModalCountries> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_registration);
 
+        arrayList = new ArrayList<>();
         getExecuteMethods();
     }
 
     void getExecuteMethods()
     {
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, AppUrl.getCountriesDataService,
-                null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                AppUrl.getCountriesDataService,
+                null, response -> {
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+            try {
+                if(response.getBoolean("status"))
+                {
+                    JSONArray jsonArray = response.getJSONArray("data");
+                    for (int i=0; i<jsonArray.length();i++)
+                    {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        ModalCountries modal = new ModalCountries();
+                        modal.setId(jsonObject.getString("id"));
+                        modal.setCountryName(jsonObject.getString("countryName"));
+                        modal.setCountryStatus(jsonObject.getString("countryStatus"));
+//                        modal.countryFlag(jsonObject.getString("countryStatus"));
+                        modal.setCountryCode(jsonObject.getString("countryCode"));
+                        modal.setCallingCode(jsonObject.getString("callingCode"));
 
+                        arrayList.add(modal);
+
+                    }
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
+
+
+        }, error -> {
+
+                });
 
         RequestQueue queue = Volley.newRequestQueue(RegistrationActivity.this);
         request.setRetryPolicy(new DefaultRetryPolicy(20*2000, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         queue.add(request);
+    }
+
+
+    void listCountries()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.custom, viewGroup, false);
+        builder.setView(dialogView);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
